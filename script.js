5dd0f66819b21b98ebcceb49b3d80a31aa6239b6
Diego Moreira <diegouezotads@gmail.com>
2023-09-14 06:49:30 -0300

    feat: those commits are the try to fix video load

--- script.js
+++ script.js
@@ -57,6 +57,8 @@ const getInformationFromAPI = async (inputDate) => {
             
             console.log (data.url);
             // videoApi.setAttribute("src", data.url)//`https://www.youtube.com/embed/=${data.url.substring(30,41)}`);
+
+            
             videoApi.setAttribute ("src", data.url)
             imageApi.classList.add("hide");
         
